135f46e61225092453ecd5338815ed3141984012
Valdenio <sgtferreirasouza@gmail.com>
2023-04-21 09:43:04 -0300

    Adicionando uma nova Exception para travar post passando Id existentes

--- src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
+++ src/main/java/com/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -1,5 +1,6 @@
 package com.client.ws.rasmooplus.exception.handler;
 
+import com.client.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.rasmooplus.exception.NotFoudException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,12 @@ public class ResourceHandler {
     @ExceptionHandler(NotFoudException.class)
     public ResponseEntity<String> notFoundException(NotFoudException n) {
         String errorMassage = n.getMessage();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMassage);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> badRequestException(BadRequestException b) {
+        String errorMassage = b.getMessage();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorMassage);
     }
 }
--- src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionsTypeServiceImpl.java
+++ src/main/java/com/client/ws/rasmooplus/service/impl/SubscriptionsTypeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.client.ws.rasmooplus.service.impl;
 
 import com.client.ws.rasmooplus.dto.SubscriptionsTypeDto;
+import com.client.ws.rasmooplus.exception.BadRequestException;
 import com.client.ws.rasmooplus.exception.NotFoudException;
 import com.client.ws.rasmooplus.model.SubscriptionsType;
 import com.client.ws.rasmooplus.repository.SubscriptionsTypeRepository;
@@ -8,6 +9,7 @@ import com.client.ws.rasmooplus.service.SubscritionsTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -35,6 +37,9 @@ public class SubscriptionsTypeServiceImpl implements SubscritionsTypeService {
 
     @Override
     public SubscriptionsType create(SubscriptionsTypeDto dto) {
+        if (Objects.nonNull(dto.getId())) {
+            throw new BadRequestException("Id deve ser nulo");
+        }
         return subscriptionsTypeRepository.save(SubscriptionsType.builder()
                         .id(dto.getId())
                         .name(dto.getName())
